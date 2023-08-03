bdb1e81f90234bdfcac8ac5c88e002fbc56159d3
Roteesaimai <107842127+Roteesaimai1@users.noreply.github.com>
2023-08-03 19:32:21 +0700

    Add api

--- routes/employee.js
+++ routes/employee.js
@@ -4,10 +4,11 @@ const db = require('../db');
 
 
 //get all user 
-router.get('/employee', (req, res) => {
-  const query = 'SELECT e.id, e.name, e.title, SUM(f.money) AS total_amount, SUM(f.days) AS total_days FROM employee e JOIN food_stamp f ON e.id = f.employee_id WHERE f.date_stamp BETWEEN "2023-07-20" AND "2023-08-30" GROUP BY e.id, e.name, e.title; ';
+router.get('/employee/:month', (req, res) => {
+  const month = req.params.month;
+  const query = 'SELECT e.id, e.name, e.title, SUM(f.money) AS total_amount, SUM(f.days) AS total_days FROM employee e JOIN food_stamp f ON e.id = f.employee_id WHERE MONTH(f.date_stamp) = ?  GROUP BY e.id, e.name, e.title; ';
 
-  db.query(query, (err, results) => {
+  db.query(query, [month], (err, results) => {
     if (err) {
       console.error('Error executing query:', err);
       res.status(500).json({ error: 'Error executing query' });
@@ -18,15 +19,18 @@ router.get('/employee', (req, res) => {
   })
 })
 
-
-
-//Get user from id
-router.get('/employee/nowtime', (req, res) => {
-  
-  const currentDate = new Date();
-  currentDate.setHours(currentDate.getHours() + 7);
-  res.json({ date: currentDate});
-
+router.get('/employeetotal/:month', (req, res) => {
+  const month = req.params.month;
+  const query = 'SELECT SUM(money) AS total_money FROM food_stamp WHERE MONTH(date_stamp) = ?';
+  db.query(query, [month], (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'Error executing query' });
+      return;
+    }
+       
+    res.json(results);   
+  })
 })
 
 
@@ -75,7 +79,7 @@ router.post('/employeecreate', (req, res) => {
 router.get('/minicard/:date', (req, res) => {
   const date = req.params.date;
   const query = 'SELECT SUM(money) AS total_date_money FROM food_stamp WHERE date_stamp = ?';
-  const querytotal = 'SELECT SUM(money) AS total_money FROM food_stamp';
+  const querytotal = 'SELECT SUM(money) AS total_money FROM food_stamp ';
   const maxtotal = 'SELECT MAX(money) AS max_money FROM food_stamp';
   const mintotal = 'SELECT MIN(money) AS min_money FROM food_stamp';
   
